60f8eedc79b09bf9b3dd61bd22135659c04f6121
Charles Christolini <charles@binarypie.com>
2013-06-04 22:59:09 -0700

    network widget is back!

--- network.lua
+++ network.lua
@@ -13,39 +13,29 @@ M.upicon:set_image(beautiful.widget_netup)
 M.downwidget = wibox.widget.textbox()
 vicious.register(M.downwidget, function(format, warg)
     local args = vicious.widgets.net(format, warg)
-    -- Wireless
-    if type(args['{wlan0 down_kb}']) ~= nil then
-        args['{down}'] = args['{wlan0 down_kb}']
-    -- Lenovo eth adapter
-    elseif type(args['{eth0 down_kb}']) ~= nil then
-        args['{down}'] = args['{eth0 down_kb}']
-    -- Apple adapter
-    elseif type(args['{enp0s20u2 down_kb}']) ~= nil then
-        args['{down}'] = args['{enp0s20u2 down_kb}']
-    -- Unknown
-    else
-        args['{down}'] = '-.-'
+    local down  = 0
+    for key,value in pairs(args) do
+        if string.find(key, 'down_kb') ~= nil then
+            down = down + args[key]
+        end
     end
+    args['{down}'] = string.format("%.2f", down)
     return args
 end, "<span>${down}</span>", 1)
+vicious.cache(M.downwidget)
 
 M.upwidget = wibox.widget.textbox()
 vicious.register(M.upwidget, function(format, warg)
     local args = vicious.widgets.net(format, warg)
-    -- Wireless
-    if type(args['{wlp3s0 up_kb}']) ~= nil then
-        args['{up}'] = args['{wlan0 up_kb}']
-    -- Lenovo eth adapter
-    elseif type(args['{enp0s20u2 down_kb}']) ~= nil then
-        args['{up}'] = args['{enp0s20u2 up_kb}']
-    -- Apple adapter
-    elseif type(args['{eth0 down_kb}']) ~= nil then
-        args['{up}'] = args['{eth0 up_kb}']
-    -- Unknown
-    else
-        args['{up}'] = '-.-'
+    local up  = 0
+    for key,value in pairs(args) do
+        if string.find(key, 'up_kb') ~= nil then
+            up = up + args[key]
+        end
     end
+    args['{up}'] = string.format("%.2f", up)
     return args
 end, "<span>${up}</span>", 1)
+vicious.cache(M.upwidget)
 
 return M
